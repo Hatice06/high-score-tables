42aabf44f411dcfcbd86338a2b1649b0efa5804d
Hatice06 <haticeaydogan00@gmail.com>
2022-04-01 19:56:21 +0100

    empty main branch to create PR from level-2

--- src/AllCountriesCards.js
+++ src/AllCountriesCards.js
@@ -1,17 +1,1 @@
-import React from "react";
-import CountryScoresCard from "./CountryScoresCard";
 
-const AllCountriesCard = (props) => {
-  return (
-    <div className="score-data-container">
-      <h1>High Scores per Country</h1>
-      {props.allCountryScores
-        .sort((a, b) => (a.name > b.name ? 1 : -1))
-        .map((country) => (
-          <CountryScoresCard country={country} />
-        ))}
-    </div>
-  );
-};
-
-export default AllCountriesCard;
