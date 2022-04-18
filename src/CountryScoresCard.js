21c59d5f970ad59743aeac6db8806957b83f81d2
Hatice06 <haticeaydogan00@gmail.com>
2022-04-18 14:27:33 +0100

    Level 4 - added button low to high

--- src/CountryScoresCard.js
+++ src/CountryScoresCard.js
@@ -5,21 +5,18 @@ const CountryScoresCard = (props) => {
     <div className="country-score-card">
       <p>HIGH SCORES: {props.country.name} </p>
       <div className="card-info">
-        {props.country.scores
-          .sort((a, b) => (a.s < b.s ? 1 : -1))
-          .map((person) => (
-            <div>
-              <div className="person-data">
-                <span className="person-name">{person.n.toUpperCase()}</span>
-                <span className="person-score">{person.s}</span>
-              </div>
-              <hr />
+        {props.country.scores.map((person) => (
+          <div>
+            <div className="person-data">
+              <span className="person-name">{person.n.toUpperCase()}</span>
+              <span className="person-score">{person.s}</span>
             </div>
-          ))}
+            <hr />
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 export default CountryScoresCard;
-
